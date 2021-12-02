0c0a1777b5b2108a2d0d4fda0a954ec3fd7a8450
Tomas <tomas.hulina@student.tuke.sk>
2021-12-02 17:37:22 +0100

    Fixes
    Opravil som chybu v počítaní, odstránil som zbytočné medzery. Upravil som aj kedy pridávame max/min

--- EA.py
+++ EA.py
@@ -1,32 +1,26 @@
 takeInput = True
 numbers=""
-
-maxSize=int(100)
+maxSize=int(199)
 minSize=int(0)
-
-
-      
-def left(numbers,position):
+   
+def low(numbers,position):
     distance=0
-    for i in reversed(range(position)):
+    for i in reversed(range(position+1)):
         distance += numbers[i+1]-numbers[i]
         #print(numbers[i+1],'-',numbers[i]," = ",numbers[i+1]-numbers[i])
         numbers.remove(numbers[i+1])
-        
     for i in range(len(numbers)):
-            if(numbers[i+1]==maxSize):
+            if(numbers[i]==maxSize):
                 break;
             else:
                 distance += numbers[i+1]-numbers[i]  
                 #print(numbers[i+1],'-',numbers[i]," = ",numbers[i+1]-numbers[i])
     
-    
     return(print(distance))
 
 
-def right(numbers,position):
-    distance=0
-    
+def high(numbers,position):
+    distance=0  
     for i in range(position,len(numbers)):
         if numbers[i] == maxSize:
             break;
@@ -34,12 +28,11 @@ def right(numbers,position):
             distance += numbers[i+1]-numbers[i]
             #print(numbers[i+1],'-',numbers[i]," = ",numbers[i+1]-numbers[i])
         
-    numbers=numbers[:position]
-    
-    numbers.insert(len(numbers)+1,maxSize)  
-    
+    numbers=numbers[:position]   
+    numbers.insert(len(numbers)+1,maxSize)
+  
     for i in reversed(range(len(numbers))):
-        if numbers[i] == minSize:
+        if numbers[i] == minSize or i == 0:
             break;
         else:
             distance += numbers[i]-numbers[i-1]
@@ -49,20 +42,27 @@ def right(numbers,position):
     return(print(distance))   
 
 
-while takeInput == True:
-    
+
+
+while takeInput == True: 
     print("######################")
     print("Hello user, please enter the numbers for the Elevator algorithm.")
     print("Separate them with space and once you are done press enter:")
     print("You can also write 'quit' to exit the program.")
-    userinput1= input("Numbers between 0 and 100: ")
+    userinput1= input("Numbers between 0 and 199: ")
     
-    if userinput1 =="quit":
+    if userinput1 =="quit":  
         takeInput=False
         break;
-
-    numbers=userinput1.split(" ")
-    for i in range(0,len(numbers)):
+    numbers=userinput1.split(" ") #rozdelenie čisel do array pri medzerách
+    numbers2=[]
+    for i in range(0,len(numbers)):#odstráneni zbytočných whitespace a iných znakov
+        if numbers[i].isnumeric():
+            numbers2.append(numbers[i])
+    
+    numbers=numbers2
+    
+    for i in range(0,len(numbers)): #ak je nejaké číslo väčise ako max tak skončí program
         numbers[i]=int(numbers[i])
         if(int(numbers[i])>maxSize or int(numbers[i])<minSize):
             userinput1 ="quit"
@@ -72,28 +72,18 @@ while takeInput == True:
         takeInput=False
         break;
 
-
-    if minSize not in numbers:
-        numbers.insert(0,minSize)
-
-    if maxSize not in numbers:
-        numbers.insert(len(numbers)+1,maxSize)
-        
-        
-    numbers.sort()
+    numbers.sort() 
     
     userinput2str= input("Please enter the starting position: ")
-    
     if userinput2str =="quit":
         takeInput=False
         break;
     
+      
     userinput2=int(userinput2str)
-    
     
     if (userinput2>maxSize or userinput2<minSize ):
         break;
-    
 
     for i in range(len(numbers)):
         if numbers[i]>userinput2:
@@ -103,43 +93,35 @@ while takeInput == True:
     numbers = numbers[:position] + [userinput2] + numbers[position:]
         
     
-    
-    
     print("Please enter the scanning direction.")
-    print("Write left to scan from right to left.")
-    print("Write right to scan from left to right:")
-    print("if neither direction is chosen the program will move from left to right.")
+    print("Write high to move from lower track number to higher track.")
+    print("Write low to scan from higher track number to lower track.")
+    print("if neither direction is chosen the program will move from lower track number to higher track.")
     userinput3= input("Direction: ")
-    
+    userinput3.lower()
+    userinput3.strip()
     if userinput3 =="quit":
         takeInput=False
         break;
     
-
-    if userinput3 == "left" or userinput3 == "right" :
+    if userinput3 == "high" or userinput3 == "low" :
         direction = userinput3
     else:
-        direction = "right"
+        direction = "high"
         
     #print(numbers)
     #print(userinput2)
     #print(position)
     #print(direction)
+    if maxSize not in numbers and direction == "high":
+        numbers.insert(len(numbers)+1,maxSize)
     
+    if minSize not in numbers and direction =="low" :
+        numbers.insert(0,minSize)
     
-    if direction == "right":
-        right(numbers,position)
+    if direction == "high":
+        high(numbers,position)
         
-    elif direction =="left":
-        left(numbers,position)
+    elif direction =="low":
+        low(numbers,position)
     
-    
-    
-    
-    if userinput1 =="quit":
-        takeInput=False
-        break; 
-        
-        
-        
-  
