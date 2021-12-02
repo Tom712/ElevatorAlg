d91f0b9388dc07d205b169d4f988685c2f24ceeb
Tomas <tomas.hulina@student.tuke.sk>
2021-12-02 02:49:12 +0100

    Working prototype
    Fungujúci Elevator(SCAN) algoritmus s možnosťou vybrať smer čítania

--- EA.py
+++ EA.py
@@ -1,8 +1,54 @@
-
 takeInput = True
+numbers=""
 
 maxSize=int(100)
 minSize=int(0)
+
+
+      
+def left(numbers,position):
+    distance=0
+    for i in reversed(range(position)):
+        distance += numbers[i+1]-numbers[i]
+        #print(numbers[i+1],'-',numbers[i]," = ",numbers[i+1]-numbers[i])
+        numbers.remove(numbers[i+1])
+        
+    for i in range(len(numbers)):
+            if(numbers[i+1]==maxSize):
+                break;
+            else:
+                distance += numbers[i+1]-numbers[i]  
+                #print(numbers[i+1],'-',numbers[i]," = ",numbers[i+1]-numbers[i])
+    
+    
+    return(print(distance))
+
+
+def right(numbers,position):
+    distance=0
+    
+    for i in range(position,len(numbers)):
+        if numbers[i] == maxSize:
+            break;
+        else:
+            distance += numbers[i+1]-numbers[i]
+            #print(numbers[i+1],'-',numbers[i]," = ",numbers[i+1]-numbers[i])
+        
+    numbers=numbers[:position]
+    
+    numbers.insert(len(numbers)+1,maxSize)  
+    
+    for i in reversed(range(len(numbers))):
+        if numbers[i] == minSize:
+            break;
+        else:
+            distance += numbers[i]-numbers[i-1]
+            #print(numbers[i],'-',numbers[i-1]," = ",numbers[i]-numbers[i-1])
+  
+
+    return(print(distance))   
+
+
 while takeInput == True:
     
     print("######################")
@@ -18,21 +64,24 @@ while takeInput == True:
     numbers=userinput1.split(" ")
     for i in range(0,len(numbers)):
         numbers[i]=int(numbers[i])
-        if(int(numbers[i])>maxSize):
-            userinput1 ="quit"
-        elif(int(numbers[i])<minSize):
+        if(int(numbers[i])>maxSize or int(numbers[i])<minSize):
             userinput1 ="quit"
             
     if userinput1 =="quit":
+        print("Wrong input")
         takeInput=False
         break;
 
 
+    if minSize not in numbers:
+        numbers.insert(0,minSize)
+
+    if maxSize not in numbers:
+        numbers.insert(len(numbers)+1,maxSize)
+        
+        
     numbers.sort()
-    numbers.insert(0,0)
     
-    numbers.insert(len(numbers)+1,maxSize)
-    print(numbers)
     userinput2str= input("Please enter the starting position: ")
     
     if userinput2str =="quit":
@@ -40,6 +89,20 @@ while takeInput == True:
         break;
     
     userinput2=int(userinput2str)
+    
+    
+    if (userinput2>maxSize or userinput2<minSize ):
+        break;
+    
+
+    for i in range(len(numbers)):
+        if numbers[i]>userinput2:
+            position=i
+            break
+        
+    numbers = numbers[:position] + [userinput2] + numbers[position:]
+        
+    
     
     
     print("Please enter the scanning direction.")
@@ -52,15 +115,31 @@ while takeInput == True:
         takeInput=False
         break;
     
-    
+
     if userinput3 == "left" or userinput3 == "right" :
         direction = userinput3
     else:
         direction = "right"
         
+    #print(numbers)
+    #print(userinput2)
+    #print(position)
+    #print(direction)
+    
+    
+    if direction == "right":
+        right(numbers,position)
+        
+    elif direction =="left":
+        left(numbers,position)
+    
     
     
     
     if userinput1 =="quit":
         takeInput=False
         break; 
+        
+        
+        
+  
